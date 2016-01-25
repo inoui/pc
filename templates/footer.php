4bb37b2e056cfb4812d267c0b5b19df8eafbdcb1
Tom <tom@inoui.io>
2016-01-25 15:35:58 +0100

    PC

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,14 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="banner wrap">
+    <nav class="nav-primary">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
+      endif;
+      ?>
+    </nav>
+
+    <p>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/g-logo.png" alt="" class="g-logo"/>
+        Cabinet de Podologie de Philippe Caudroy<br>Votre podologue à Issy les Moulineaux
+    </p>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,35 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+<header class="top-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                Philippe Caudroy - Pédicure - Podologue D.E
+            </div>
+            <div class="col-sm-6">
+                <ul class="list-inline">
+                    <li><a href="#">Prendre rendez-vous</a></li>
+                    <li><a href="#">Plan d'accès</a></li>
+                    <li><a href="#">Le Cabinet</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                Recherche
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<header class="banner wrap">
+
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg" alt="" />
+    </a>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
-  </div>
+
 </header>
